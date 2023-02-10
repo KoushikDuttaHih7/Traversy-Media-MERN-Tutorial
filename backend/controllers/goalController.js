1ae1037e5f4280fb93791c7e4b4025328a657da5
KoushikDuttaHih7 <koushik.dutta@hih7.in>
2023-02-10 16:08:36 +0530

    Accepting Body data

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,6 @@
 // desc  === Get Goals
 // route  === Get /api/goals
 // access  === Private
-
 const getGoals = (req, res) => {
   res.status(200).json({ message: "Get Goals" });
 };
@@ -9,15 +8,14 @@ const getGoals = (req, res) => {
 // desc  === Set Goal
 // route  === Post /api/goals
 // access  === Private
-
 const setGoal = (req, res) => {
+  console.log(req.body);
   res.status(200).json({ message: "Set Goal" });
 };
 
 // desc  === Update Goal
 // route  === Put /api/goals?:id
 // access  === Private
-
 const updateGoal = (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
 };
@@ -25,7 +23,6 @@ const updateGoal = (req, res) => {
 // desc  === Delete Goals
 // route  === Delete /api/goals
 // access  === Private
-
 const deleteGoal = (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
 };
